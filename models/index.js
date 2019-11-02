e07ff7f68a1460b8e93e0fce73788449a10d1859
ladonal <alaayoussef1997@gmail.com>
2019-11-02 22:24:20 +0400

    setup the models folder

---
+++ models/index.js
@@ -0,0 +1,15 @@
+module.exports = {
+    parent: require('./parent.js'),
+	child: require('./child.js'),
+	smartwatch: require('./smartwatch.js'),
+	atmosphere: require('./atmosphere.js'),
+	equipmentHistory: require('./equipmentHistory.js'),
+	heartRateHistory: require('./heartRateHistory.js'),
+	panicHistory: require('./panicHistory.js'),
+	objectHistory: require('./objectHistory.js'),
+	watchAlert: require('./watchAlert.js'),
+	locationHistory: require('./locationHistory.js'),
+	lightSensorHistory: require('./lightSensorHistory.js'),
+	roomTempHistory: require('./roomTempHistory.js'),
+	atmosphereAlert: require('./atmosphereAlert.js')
+};
