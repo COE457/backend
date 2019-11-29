816d1a9fc59cc3ded721748828234bd01f8e9a09
ladonal <alaayoussef1997@gmail.com>
2019-11-29 16:54:40 +0400

    add views

--- db.js
+++ db.js
@@ -1,12 +1,33 @@
-const nano = require('nano');
+/**
+ * @file db.js
+ * 
+ * @description used for initializing database settings
+ */
+const nano = require('nano'); //  to communicate with couch
 const opts = require('./config').dbOpts; //  importing db options
+const fs = require('fs'); //  for reading JSON files 
 
 const service = nano(opts.DB_URL); //  connecting to couchdb
 
 //  creating the database if it doesn't exist
-service.db.create(opts.DB_NAME).catch(err => {
-    console.log(err.error);
-})
+service.db.create(opts.DB_NAME).then(db => {
+    console.log(`${JSON.stringify(db)} successfully created!`);
+}).catch(err => {
+    if (err.error === "file_exists") {
+        console.log("database already exists. Previous entries were not affected");
+    }
+});
 
+const db = service.use(opts.DB_NAME);
 
-module.exports = service.use(opts.DB_NAME);
+const views = JSON.parse(fs.readFileSync('./config/views.json'));
+
+db.insert(views).then(views => {
+    console.log(`${views.id} successfully added to db!`);
+}).catch(err => {
+    if (err.error === "conflict") {
+        console.log("views already loaded. To update views, manually delete '_design/sorted'");
+    }
+});
+
+module.exports = db;
