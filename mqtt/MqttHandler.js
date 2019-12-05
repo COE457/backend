b8ebb85a7036cebb3fca46a1b30354dcda4f29a7
Alaa Youssef <ladonal@pop-os.localdomain>
2019-12-05 19:48:57 +0400

    mqtt works now (I think)

--- mqtt/MqttHandler.js
+++ mqtt/MqttHandler.js
@@ -1,44 +1,68 @@
-const mqtt = require('mqtt');
+const mqtt = require("mqtt");
+const models = require("../models");
+const docs = [
+  "locationHistory",
+  "heartRateHistory",
+  "lightSensorHistory",
+  "roomTempHistory"
+];
+const controllers = {};
+docs.forEach(item => {
+  const constructor = models[item];
+  controllers[item] = new constructor();
+});
 
 class MqttHandler {
   constructor() {
     this.mqttClient = null;
-    this.host = 'mqtt://broker.mqttdashboard.com';
+    this.host = "mqtt://broker.mqttdashboard.com";
     // this.username = 'YOUR_USER'; // mqtt credentials if these are needed to connect
     // this.password = 'YOUR_PASSWORD';
   }
-  
+
   connect() {
     // Connect mqtt with credentials (in case of needed, otherwise we can omit 2nd param)
-    this.mqttClient = mqtt.connect(this.host/*, { username: this.username, password: this.password }*/);
+    this.mqttClient = mqtt.connect(
+      this.host /*, { username: this.username, password: this.password }*/
+    );
 
     // Mqtt error calback
-    this.mqttClient.on('error', (err) => {
-      console.log(err);
+    this.mqttClient.on("error", err => {
+      console.error(err);
       this.mqttClient.end();
     });
 
     // Connection callback
-    this.mqttClient.on('connect', () => {
+    this.mqttClient.on("connect", () => {
       console.log(`mqtt client connected`);
     });
 
     // mqtt subscriptions
-    this.mqttClient.subscribe('sensors/#', {qos: 0});
+    this.mqttClient.subscribe("sensors/#", { qos: 0 });
 
     // When a message arrives, console.log it
-    this.mqttClient.on('message', function (topic, message) {
-      console.log(`${topic}: ${message.toString()}`);
+    this.mqttClient.on("message", function(topic, message) {
+      message = message.toString();
+      if (docs.includes(topic.split("/")[1])) {
+        controllers[topic.split("/")[1]]
+          .create(JSON.parse(message))
+          .then(l => {
+            console.log("l: ", l);
+          })
+          .catch(err => {
+            console.log("err: ", err);
+          });
+      }
     });
 
-    this.mqttClient.on('close', () => {
+    this.mqttClient.on("close", () => {
       console.log(`mqtt client disconnected`);
     });
   }
 
   // Sends a mqtt message to topic: mytopic
   sendMessage(message) {
-    this.mqttClient.publish('sensors/backend', message);
+    this.mqttClient.publish("sensors/backend", message);
   }
 }
 
